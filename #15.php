487c1a2114ef3a483454b3473508ca90c96fffcd
Kolya <kelbar6@gmail.com>
2017-06-25 19:59:02 +0300

    Add PHP homework

---
+++ #15.php
@@ -0,0 +1,38 @@
+<?php
+//15. Написать калькулятор. Переменная $a = изменяемое число.
+// Переменная $b = изменяемое число. Переменная $operator = ‘+’ или ‘-’ или ‘/’ или ‘*’ или '%' (остаток от деления).
+//На экран выводить результат в зависимости от значений этих переменных.
+//Не забудьте про деление на 0, если надо - выдавать ошибку что на 0 делить нельзя.
+
+$a=55;
+$b=3;
+$operator= '%';
+
+switch ($operator):
+    case '+':
+        $result=$a + $b;
+        echo $a,' + ',$b,' = ',$result;
+        break;
+    case '-':
+        $result=$a - $b;
+        echo $a,' - ',$b,' = ',$result;
+        break;
+    case '*':
+        $result=$a * $b;
+        echo $a,' * ',$b,' = ',$result;
+        break;
+    case '/':
+        if($b == 0){
+            echo "Exception devided by 0";}
+        else{
+            $result=$a / $b;
+            echo $a,' / ',$b,' = ',$result;}
+        break;
+    case '%':
+        $result=$a % $b;
+        echo $a,' % ',$b,' = ',$result;
+        break;
+    default:
+        echo "Incorrect operator, try more";
+endswitch;;
+
